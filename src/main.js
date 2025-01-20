a228dc81493b318b76772c32e7602b452eb6e415
Nikitin <nikitinigor0907@gmail.com>
2025-01-20 14:24:44 +0100

    header

--- src/main.js
+++ src/main.js
@@ -3,17 +3,6 @@ import { gallery } from './assets/js/gallery.js';
 import { burger } from './assets/js/menu.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  function loadPartial(url, elementId) {
-    fetch(url)
-      .then(response => response.text())
-      .then(html => {
-        document.getElementById(elementId).innerHTML = html;
-      })
-      .catch(err => console.warn('Error loading partial:', err));
-  }
-
-  loadPartial('/partials/header.html', 'header');
-
   if (window.innerWidth < 1200) {
     advantages();
   }
